82d2b97b2ea31e95b4f8391a1bf8a8e06d0ff170
pezypezy <60894556+pezypezy@users.noreply.github.com>
2026-06-29 17:44:20 +0900

    Keep locale home page typed locally

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,7 +1,7 @@
 import { setRequestLocale } from "next-intl/server";
 import { Picker } from "@/components/Picker";
 
-export default async function Home({ params }: PageProps<"/[locale]">) {
+export default async function Home({ params }: { params: Promise<{ locale: string }> }) {
   const { locale } = await params;
   setRequestLocale(locale);
   return <Picker />;
